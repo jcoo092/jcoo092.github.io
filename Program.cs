21cc40ffda3ce00d77c5d801f840230fcd6d2d85
James Cooper <30380166+jcoo092@users.noreply.github.com>
2023-07-10 15:05:03 +1200

    Revert "Deployment from Statiq"
    This reverts commit 49c33f177944425a1d8dcc81ae8c1c61670963ba.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿return await Bootstrapper
+    .Factory
+    .CreateWeb(args)
+    .DeployToGitHubPages(
+        "jcoo092",
+        "jcoo092.github.io",
+        Config.FromSetting<string>("GITHUB_TOKEN")
+    )
+    .RunAsync();
---
+++ input/posts/owasp-juice-shop-deploying-securing-1.md
@@ -0,0 +1,10 @@
+Title: Deploying Then Securing the OWASP Juice Shop, Part One
+Lead: Explaining what I plan
+Published: 09-07-2023
+Tags:
+
+- AppSec
+- OWASP
+
+---
+Wherein I explain my plannerising.
